cdd72bc6bcf65fb0dc47784326e4b3653470dc7f
AleitaT <traina@oregonstate.edu>
2018-04-07 14:07:58 -0700

    quotes and things

--- src/routes_frontend.js
+++ src/routes_frontend.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const db = require('./db.js');
 /**
  * This function estableshes routes for the White Bird Help Book Frontend
  * 
@@ -31,6 +31,38 @@ function routes(app) {
 
 	app.get('/emergency', (req, res) => {
 	  res.render('emergency');
+	});
+
+
+	// call to listings should make an api request to reutrn 
+	// all service listings which should then be indexed alphabetically 
+	app.get('/listing', (req, res) => {
+	  db.models.service.find().exec(function(data) {
+	  	res.status(200).json({
+	  		status: 'success',
+	  		data: data,
+	  		message: "Retrieved all listings from subjects"
+	  	});
+	  }).catch(function(err) {
+	  	console.log("error", err);
+	  })
+	  res.render('listing', JSON.parse(response.body));
+	});
+
+	// call to listing: lid should request all data and return category 
+	// specific information
+	app.get('/listing/:id', (req, res) => {
+	  let category = req.params.id;
+	  db.models.service({category}).then(function(data) {
+	  	res.status(200).json({
+	  		status: 'success',
+	  		data: data,
+	  		message: "Retrieved category  from subjects"
+	  	});
+	  }).catch(function(err) {
+	  	console.log('error'. err);
+	  })
+	  res.render('listing', JSON.parse(response.body));
 	});
 }
 
