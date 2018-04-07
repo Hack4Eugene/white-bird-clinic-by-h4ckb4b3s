6671567dd018b2ffea3c9b6a2a15abaf9fa1995a
Joshua Evans <TheJoshuaEvans@gmail.com>
2018-04-07 14:33:08 -0700

    Seeding

--- utils/seed.js
+++ utils/seed.js
@@ -3,21 +3,58 @@
 /**
  * This script is used to seed the Help Book database for testing purposes
  */
+const async = require('async');
 
 const db = require('../src/db.js');
 const Data = require('../src/data.js');
 
 const DATA_CSV_LOCATION = __dirname + '/../test/assets/data_sample_services.csv';
+const REPORT_MODULUS = 50;
 
 db.connect('mongodb://localhost/white_bird').then((connection) => {
-  // Get data
-  var data = new Data(null, DATA_CSV_LOCATION, () => {
-    // Data is ready!
-    data.data.forEach((value) => {
-      // Add the data to the database with upserts
-      
+  var Service = db.models.service;
+  
+  // Clear all data
+  Service.remove({}).then((adventure) => {
+    console.log(`Removed ${adventure.n} Elements`);
+    // Get data
+    var data = new Data(null, DATA_CSV_LOCATION, () => {
+      // Data is ready!
+      var saveCount = 0;
+      var tasks = data.data.map((value) => {
+        return function(callback) {
+          // Save the data to the database
+          var newService = new Service(value);
+
+          newService.save().then((adventure) => {
+            saveCount++;        
+            if (saveCount % REPORT_MODULUS === 0) {
+              console.log(`Saved ${saveCount} services. ${((saveCount / data.data.length) * 100).toFixed(2)}%`)
+            }
+
+            callback(null, true);
+          }).catch((err) => {
+            console.error(`Error saving model to database: ${err}`);
+            callback(err);
+          });
+        }
+      });
+
+      async.parallel(tasks, (err, res) => {
+        if (err) {
+          console.error('Error detected');
+        }
+
+        console.log('Finished Seeding!');
+
+        db.disconnect((err) => {
+          console.log('Databse Disconnected - process finished')
+        });
+      });
     });
-  });
+  }).catch((err) => {
+    console.error('Error deleting existing elements');
+  });  
 }).catch((err) => {
   console.error('Error Connecting to database for seeding');
 
